3031012f8e1ef376d62bfe974bc0cf181ad70391
Muhammad Yasir <imyasir79@yahoo.com>
2023-12-18 20:44:58 +0500

    Design of Salary page updated a little

--- src/app/salary/page.tsx
+++ src/app/salary/page.tsx
@@ -29,35 +29,38 @@ import {
 
 const taxSlab = [
   { id: 1, min: 0, max: 600000, fixTax: 0, taxRate: 0 },
-  { id: 2, min: 600001, max: 1200000, fixTax: 5000, taxRate: 0.025 },
-  { id: 3, min: 1200001, max: 2400000, fixTax: 10000, taxRate: 0.05 },
-  { id: 4, min: 2400001, max: 3600000, fixTax: 15000, taxRate: 0.075 },
-  { id: 5, min: 3600001, max: 9999999999, fixTax: 20000, taxRate: 0.01 }
+  { id: 2, min: 600001, max: 1200000, fixTax: 0, taxRate: 0.025 },
+  { id: 3, min: 1200001, max: 2400000, fixTax: 15000, taxRate: 0.125 },
+  { id: 4, min: 2400001, max: 3600000, fixTax: 165000, taxRate: 0.225 },
+  { id: 5, min: 3600001, max: 6000000, fixTax: 435000, taxRate: 0.275 },
+  { id: 6, min: 6000001, max: 999999999999, fixTax: 1095000, taxRate: 0.35 }
 ];
 
 const taxCodeSalary = [
-  { id: 1, status: 'Government', section: '149(1)', code: '00010101' },
-  { id: 2, status: 'Corporate', section: '149(2)', code: '00020102'  },
-  { id: 3, status : 'Others', section: '149(3)', code: '00030103' },
+  { id: 1, status: 'Federal Govt', section: '149/1', nature: 'Adjustable' , code: '64020001' },
+  { id: 2, status: 'Provincial Govt', section: '149/2', nature: 'Adjustable', code: '64020002'  },
+  { id: 3, status : 'Other', section: '149/3',nature: 'Adjustable', code: '64020004' },
 ]
 
 
 const Salary = () => {
   const [salary, setSalary] = useState<number>(0);
-  const [selectedEmploymentStatus, setSelectedEmploymentStatus] = useState(null);
+  const [selectedEmploymentStatus, setSelectedEmploymentStatus] = useState(null || '');
 
   const [annualSal, setAnnualSal] = useState<number>(0);
   const [result, setResult] = useState<{ id?: number, min?: number, max?: number, 
         fixTax?: number; taxRate?: number }>({});
   const [totalTax, setTotalTax] = useState<{fixedTax: number, varTax: number}>({fixedTax: 0, varTax: 0});
-  const [taxCode, setTaxCode] = useState<{id?: number, status?: string, section?: string, code?: string}>({});
+  const [taxCode, setTaxCode] = useState<{id?: number, status?: string, section?: string, nature?: string ,code?: string}>({});
   
   const taxCalc = (salary: number, selectedEmploymentStatus: string) => {
     let annualSal = salary * 12;
     let result: {id?: number, min?: number, max?: number, 
                 fixTax?: number; taxRate?: number } = {};
     let tax : {fixedTax: number; varTax: number};
-    let taxCode: {id?: number, status?: string, section: string , code?: string};
+    let taxCode: {id?: number, status?: string, section: string, nature?: string ,code?: string} = {
+      section: ''
+    };
 
     for (const slab of taxSlab) {
       if (annualSal >= slab.min && annualSal <= slab.max) {
@@ -71,7 +74,7 @@ const Salary = () => {
 
     for(const code of taxCodeSalary){
       if(selectedEmploymentStatus === code.status){
-        taxCode = {id: code.id, status: code.status, section: code.section , code: code.code,};
+        taxCode = {id: code.id, status: code.status, section: code.section, nature: code.nature, code: code.code,};
         break;
       }
     }
@@ -115,13 +118,13 @@ const Salary = () => {
           
           onValueChange={(e) => setSelectedEmploymentStatus(e)}
           >
-            <SelectTrigger className="w-[250px] my-2">
+            <SelectTrigger className="w-[250px] my-2 shadow-xl">
               <SelectValue placeholder="Employment Status?" />
             </SelectTrigger>
             <SelectContent > 
-              <SelectItem value="Government">Government</SelectItem>
-              <SelectItem value="Corporate">Corporate</SelectItem>
-              <SelectItem value="Others">Others</SelectItem>
+              <SelectItem value="Federal Govt">Federal Govt</SelectItem>
+              <SelectItem value="Provincial Govt">Provincial Govt</SelectItem>
+              <SelectItem value="Other">Other</SelectItem>
             </SelectContent>
           </Select>
 
@@ -132,7 +135,7 @@ const Salary = () => {
         <div>
 
           <Input           
-          className="border w-[250px]"
+          className="border w-[250px] shadow-xl"
           type="text"
           placeholder='Enter Per Month Salary'
           pattern="[0-9]*"
@@ -141,13 +144,14 @@ const Salary = () => {
          
         </div>
         
-        <Button className='mt-2 w-[250px]'
+        <Button className='mt-2 w-[250px] shadow-xl'
         onClick={() => handleClick(salary, selectedEmploymentStatus)}
         >Calculate</Button>
 
-          <div className='mt-5'>
-            <h1 className='text-center text-gray-400'>Witholding Tax Calculation </h1>
-            <Table>
+          <div className='mt-5 shadow-2xl'>
+            <h1 className='text-center text-gray-400 '>Witholding Tax Calculation </h1>
+
+            <Table className='w-[400px] '>
               {/* <TableCaption>Witholding Tax Calculation</TableCaption> */}
                   <TableHeader>
                     <TableRow>
@@ -174,12 +178,12 @@ const Salary = () => {
                       <TableCell className="text-right">{totalTax.varTax.toLocaleString()}</TableCell>
                      </TableRow>
                      <TableRow>
-                      <TableCell className="font-medium">Total Tax</TableCell>
-                      <TableCell className="text-right">{(totalTax.fixedTax + totalTax.varTax).toLocaleString()}</TableCell>
+                      <TableCell className=" font-bold">Total Tax</TableCell>
+                      <TableCell className="text-right font-bold">{(totalTax.fixedTax + totalTax.varTax).toLocaleString()}</TableCell>
                      </TableRow>
                      <TableRow>
-                      <TableCell className="font-medium">Monthly Deductions</TableCell>
-                      <TableCell className="text-right">{(((totalTax.fixedTax + totalTax.varTax) / 12).toFixed(0)).toLocaleString()}</TableCell>
+                      <TableCell className="font-bold text-rose-600">Monthly Deduction</TableCell>
+                      <TableCell className="text-right font-bold text-rose-600">{(((totalTax.fixedTax + totalTax.varTax) / 12).toFixed(0)).toLocaleString()}</TableCell>
                      </TableRow>
               </TableBody>
             </Table>
@@ -189,13 +193,14 @@ const Salary = () => {
               {/* <TableCaption>Information pertains to PSID eFiling</TableCaption> */}
                   
                 <TableBody>
-                    {/* <TableRow>
-                      <TableCell className="font-medium">Employment Status</TableCell>
-                      <TableCell className="text-right">{taxCode.status}</TableCell>
-                     </TableRow> */}
+                    
                      <TableRow>
                       <TableCell className="font-medium">Payment Section(PSID)</TableCell>
                       <TableCell className="text-right">{taxCode.section}</TableCell>
+                     </TableRow>
+                     <TableRow>
+                      <TableCell className="font-medium">Payment Nature</TableCell>
+                      <TableCell className="text-right">{taxCode.nature}</TableCell>
                      </TableRow>
                      <TableRow>
                       <TableCell className="font-medium">Tax Code (eFiling)</TableCell>
