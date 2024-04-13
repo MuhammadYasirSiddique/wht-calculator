9cedf88bd9b5ce6a42f84896af3ea0f1376f2652
MuhammadYasir <imyasir79@yahoo.com>
2024-04-14 01:38:41 +0500

    API Link updated - WHT Business u/s 153 completed. except certain services

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -22,8 +22,8 @@ interface TaxSlabBusiness {
 const getTaxSlabBusiness = async () => {
   try {
     const res = await fetch(
-      // "https://wht-calculator.vercel.app/api/taxslabbusiness",
-      "http://localhost:3000/api/taxslabbusiness",
+      "https://wht-calculator.vercel.app/api/taxslabbusiness",
+      // "http://localhost:3000/api/taxslabbusiness",
       {
         method: "GET",
         cache: "no-store",
